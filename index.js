f9be8cdbd6055b12e6e5bd99f7b9199422141ea2
Kevin <kevin1.glaentzlin@epitech.eu>
2023-02-06 15:39:42 +0100

    [-] remove file because the are mooved in folder

--- index.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-import pkg from 'discord.js';
-
-dotenv.config();
-const { Client, Intents, MessageEmbed, ThreadMemberFlags, GuildScheduledEvent, PermissionsBitField} = pkg;
-const bot = new Client({ intents: 32767 });
-const token = process.env.TOKEN;
-
-bot.login(token);
