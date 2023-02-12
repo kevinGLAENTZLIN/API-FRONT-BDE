9c7b142e747182afff4a78a28e1873cb958c4da1
Kevin <kevin1.glaentzlin@epitech.eu>
2023-02-12 22:09:49 +0100

    [+] change in api/users/routes.js app. by router. for propertely and change in index.js require('express')(app) by juste one app.use with all routes

--- api/index.js
+++ api/index.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var app = express();
 var connect_database = require('./DataBase/database.js')
+var routing = require('./users/routes.js');
 const app_port = process.env.APP_PORT;
 
 connect_database;
-require('./users/routes.js')(app);
+app.use('/item', routing);
 
 app.listen(app_port, () => {
  console.log("Serveur connecté sur le port :", app_port);
--- api/users/routes.js
+++ api/users/routes.js
@@ -1,51 +1,52 @@
-module.exports = app => {
+const request_sql = require('../DataBase/database.js');
+const BodyParser = require('body-parser');
+var JsonParser = BodyParser.json();
+var express = require('express');
+var router = express.Router()
 
-    const request_sql = require('../DataBase/database.js');
-    const BodyParser = require('body-parser');
-    var JsonParser = BodyParser.json();
-
-    app.post('/item', JsonParser, (req, res) => {
-        const newItem = {
-            nom: req.body.nom,
-            nombres: req.body.nombres,
-            reférence: req.body.reférence,
-            prix: req.body.prix
+router.post('/', JsonParser, (req, res) => {
+    const newItem = {
+        nom: req.body.nom,
+        nombres: req.body.nombres,
+        reférence: req.body.reférence,
+        prix: req.body.prix
+    }
+    request_sql.query(`INSERT INTO inventaire VALUES ("${newItem.nom}", "${newItem.nombres}", "${newItem.reférence}", "${newItem.prix}")`, (err, res) => {
+        if (err) {
+            console.log("erreur: ", err);
+            return;
         }
-        request_sql.query(`INSERT INTO inventaire VALUES ("${newItem.nom}", "${newItem.nombres}", "${newItem.reférence}", "${newItem.prix}")`, (err, res) => {
-              if (err) {
-                console.log("erreur: ", err);
-                return;
-            }
-            console.log("création d'un nouvel item: ", {newItem});
-        });
-        res.send(200);
+        console.log("création d'un nouvel item: ", { newItem });
     });
+    res.send(200);
+});
 
-    app.get('/item', (req, res) => {
-        request_sql.query('SELECT * FROM inventaire', (err, res) => {
-            if (err) {
-                console.log("erreur: ", err);
-                return;
-            }
-            console.log("Database bien affiché");
-        });
-        res.send(200);
-    });
-
-    app.put('/item', JsonParser, (req, res) => {
-        const newItem = {
-            nom: req.body.nom,
-            nombres: req.body.nombres,
-            reférence: req.body.reférence,
-            prix: req.body.prix
+router.get('/', (req, res) => {
+    request_sql.query('SELECT * FROM inventaire', (err, res) => {
+        if (err) {
+            console.log("erreur: ", err);
+            return;
         }
-        request_sql.query(`UPDATE inventaire SET nom = "${newItem.nom}", nombres = "${newItem.nombres}", reférence = "${newItem.reférence}", prix = "${newItem.prix}")`, (err, res) => {
-            if (err) {
-                console.log("erreur :", err)
-                return;
-            }
-            console.log("Table bien mise à jour")
-        });
-        res.send(200);
-    })
-};
+        console.log("Database bien affiché");
+    });
+    res.send(200);
+});
+
+router.put('/', JsonParser, (req, res) => {
+    const newItem = {
+        nom: req.body.nom,
+        nombres: req.body.nombres,
+        reférence: req.body.reférence,
+        prix: req.body.prix
+    }
+    request_sql.query(`UPDATE inventaire SET nom = "${newItem.nom}", nombres = "${newItem.nombres}", reférence = "${newItem.reférence}", prix = "${newItem.prix}")`, (err, res) => {
+        if (err) {
+            console.log("erreur :", err)
+            return;
+        }
+        console.log("Table bien mise à jour")
+    });
+    res.send(200);
+})
+
+module.exports = router;
