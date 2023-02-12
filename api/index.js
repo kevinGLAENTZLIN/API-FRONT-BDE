10e30f66bd79c31e6625a1c66dff1d5c3c79b5d1
Kevin <kevin1.glaentzlin@epitech.eu>
2023-02-12 21:56:54 +0100

    [+] feat: add 3 request sql with api get put and post

--- api/index.js
+++ api/index.js
@@ -1,14 +1,10 @@
 var express = require("express");
 var app = express();
 var connect_database = require('./DataBase/database.js')
-const BodyParser = require('body-parser');
-require('./users/routes.js')(app);
 const app_port = process.env.APP_PORT;
 
-app.use(BodyParser.json());
-app.use(BodyParser.urlencoded({ extended: true }));
-
 connect_database;
+require('./users/routes.js')(app);
 
 app.listen(app_port, () => {
  console.log("Serveur connecté sur le port :", app_port);
--- api/users/routes.js
+++ api/users/routes.js
@@ -1,14 +1,51 @@
 module.exports = app => {
-    const item = require('./request.js');
 
-    app.post('/item', (req, res) => {
-        console.log("req = ", req);
-        const item = {
+    const request_sql = require('../DataBase/database.js');
+    const BodyParser = require('body-parser');
+    var JsonParser = BodyParser.json();
+
+    app.post('/item', JsonParser, (req, res) => {
+        const newItem = {
             nom: req.body.nom,
             nombres: req.body.nombres,
-            ref: req.body.ref,
+            reférence: req.body.reférence,
             prix: req.body.prix
         }
-        item.create(item);
+        request_sql.query(`INSERT INTO inventaire VALUES ("${newItem.nom}", "${newItem.nombres}", "${newItem.reférence}", "${newItem.prix}")`, (err, res) => {
+              if (err) {
+                console.log("erreur: ", err);
+                return;
+            }
+            console.log("création d'un nouvel item: ", {newItem});
+        });
+        res.send(200);
     });
+
+    app.get('/item', (req, res) => {
+        request_sql.query('SELECT * FROM inventaire', (err, res) => {
+            if (err) {
+                console.log("erreur: ", err);
+                return;
+            }
+            console.log("Database bien affiché");
+        });
+        res.send(200);
+    });
+
+    app.put('/item', JsonParser, (req, res) => {
+        const newItem = {
+            nom: req.body.nom,
+            nombres: req.body.nombres,
+            reférence: req.body.reférence,
+            prix: req.body.prix
+        }
+        request_sql.query(`UPDATE inventaire SET nom = "${newItem.nom}", nombres = "${newItem.nombres}", reférence = "${newItem.reférence}", prix = "${newItem.prix}")`, (err, res) => {
+            if (err) {
+                console.log("erreur :", err)
+                return;
+            }
+            console.log("Table bien mise à jour")
+        });
+        res.send(200);
+    })
 };
