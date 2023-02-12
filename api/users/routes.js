2d395e9152a358bae81cba5225b16d833c0f9bdc
Kevin <kevin1.glaentzlin@epitech.eu>
2023-02-12 23:32:24 +0100

    [+] feat: new command api for delete line and update line

--- api/users/routes.js
+++ api/users/routes.js
@@ -2,6 +2,7 @@ const request_sql = require('../DataBase/database.js');
 const BodyParser = require('body-parser');
 var JsonParser = BodyParser.json();
 var express = require('express');
+const { json } = require('express');
 var router = express.Router()
 
 router.post('/', JsonParser, (req, res) => {
@@ -13,10 +14,10 @@ router.post('/', JsonParser, (req, res) => {
     }
     request_sql.query(`INSERT INTO inventaire VALUES ("${newItem.nom}", "${newItem.nombres}", "${newItem.reférence}", "${newItem.prix}")`, (err, res) => {
         if (err) {
-            console.log("erreur: ", err);
+            console.log("error: ", err);
             return;
         }
-        console.log("création d'un nouvel item: ", { newItem });
+        console.log("New item create: ", { newItem });
     });
     res.send(200);
 });
@@ -24,27 +25,42 @@ router.post('/', JsonParser, (req, res) => {
 router.get('/', (req, res) => {
     request_sql.query('SELECT * FROM inventaire', (err, res) => {
         if (err) {
-            console.log("erreur: ", err);
+            console.log("error: ", err);
             return;
         }
-        console.log("Database bien affiché");
+        console.log("Database well displayed");
     });
     res.send(200);
 });
 
 router.put('/', JsonParser, (req, res) => {
-    const newItem = {
+    const UpdateItem = {
         nom: req.body.nom,
         nombres: req.body.nombres,
         reférence: req.body.reférence,
         prix: req.body.prix
     }
-    request_sql.query(`UPDATE inventaire SET nom = "${newItem.nom}", nombres = "${newItem.nombres}", reférence = "${newItem.reférence}", prix = "${newItem.prix}")`, (err, res) => {
+    request_sql.query('UPDATE inventaire SET nom = ?, nombres = ?, reférence = ?, prix = ? WHERE nom = ?',
+    [UpdateItem.nom, UpdateItem.nombres, UpdateItem.reférence, UpdateItem.prix, UpdateItem.nom], (err, res) => {
         if (err) {
-            console.log("erreur :", err)
+            console.log("error :", err)
             return;
         }
-        console.log("Table bien mise à jour")
+        console.log("Table updated: ", { UpdateItem });
+    });
+    res.send(200);
+});
+
+router.delete('/', JsonParser, (req, res) => {
+    const DeleteItem = {
+        nom: req.body.nom,
+    }
+    request_sql.query('DELETE FROM inventaire WHERE nom = ?', [DeleteItem.nom], (err, res) => {
+        if (err) {
+            console.log("error :", err)
+            return;
+        }
+        console.log("Line suceffuly delete: ", { DeleteItem })
     });
     res.send(200);
 })
